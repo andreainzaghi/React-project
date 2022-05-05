a9854f75c64e4e3fc60dd4333896b1fc2f52ea87
andreainzaghi <andreainzaghilavoro@gmail.com>
2022-05-05 14:40:59 +0200

    card-stiling

--- react-mini-project/src/pages/Dashboard/components/CardItem/CardItem.jsx
+++ react-mini-project/src/pages/Dashboard/components/CardItem/CardItem.jsx
@@ -9,23 +9,33 @@ export const CardItem = ({ pokemon }) => {
   const classes = `${pokemon.backGroundColorType}`;
 
   return (
-    <section className={`card ${classes} mt-1 mb-1`}>
-      <div className="pt-1 text-4xl text-right opacity-25">
-        <span>{generateId(pokemon.id)}</span>
+    <section className={`card ${classes} items-start justify-between`}>
+      <div className="block ">
+        <div className="flex justify-around ">
+          <div className="text-2xl text-[#fff] font-semibold uppercase">
+            <p>{pokemon.name}</p>
+          </div>
+
+        </div>
+
+
+        <div>
+          {pokemon.types.map((type, idx) => (
+            <PokemonType type={type.type.name} direction="col" key={idx} />
+          ))}
+        </div>
       </div>
-      <div className="text-2xl text-[#fff] font-semibold uppercase">
-        <p>{pokemon.name}</p>
+      <div>
+        <div className=" text-4xl opacity-25 text-center">
+          <span>{generateId(pokemon.id)}</span>
+        </div>
+        <img
+          src={pokemon.img}
+          className=" w-40 h-40 bg-no-repeat bg-cover   "
+          alt=""
+        />
       </div>
-      <div className="flex-col pt-1">
-        {pokemon.types.map((type, idx) => (
-          <PokemonType type={type.type.name} direction="col" key={idx} />
-        ))}
-      </div>
-      <img
-        src={pokemon.img}
-        className="absolute w-40 h-40 bg-no-repeat bg-cover right-10 bottom-3"
-        alt=""
-      />
+
     </section>
   );
 };
--- react-mini-project/src/pages/Dashboard/components/PokemonType/PokemonType.jsx
+++ react-mini-project/src/pages/Dashboard/components/PokemonType/PokemonType.jsx
@@ -3,7 +3,7 @@ import "./PokemonType.css";
 export const PokemonType = ({ type, direction }) => {
   return (
     <div
-      className={`border mb-1 mt-1 rounded-full w-fit p-1 bg-[#fff] opacity-50 font-semibold ${direction}`}
+      className={`border mb-1 mt-1 rounded-full w-fit px-3 py-1 bg-[#fff] opacity-50 font-semibold ${direction}`}
     >
       <p>{type}</p>
     </div>
